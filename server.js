75e9dc13868416ce1c16bbbdf82f2705475e1bdb
yiga-anonymouz <idrisadeyiga@gmail.com>
2022-11-25 00:09:54 +0100

    Added a Procfile and some updates

--- server.js
+++ server.js
@@ -137,6 +137,10 @@ app.post('/update', (req, res) => {
 
 })
 
-app.listen('3000', () =>{
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () =>{
     console.log('Server started')
 })
